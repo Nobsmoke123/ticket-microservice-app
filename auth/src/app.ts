c2debff71dc633afd808d3b59106166bb9a56f3f
Nobsmoke123 <donaldakobundu@gmail.com>
2025-05-02 00:49:32 +0100

    feat(auth): implement current user endpoint and add Kubernetes ingress configuration

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 
 const app = express();
@@ -6,5 +6,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+app.get('/api/users/currentUser', (req: Request, res: Response) => {
+    res.status(200).json({
+        message: 'Auth service is running'
+    })
+});
 
 export default app;
