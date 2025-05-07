aec5e072401792179fd60a70b7b4acaa25a908af
Nobsmoke123 <donaldakobundu@gmail.com>
2025-05-07 11:25:30 +0100

    feat(tests): add Jest testing framework and MongoDB memory server for integration tests
    - Updated package.json to include Jest, Supertest, and MongoDB Memory Server dependencies.
    - Added test scripts for running tests and watching for changes.
    - Created setup file for Jest to initialize MongoDB Memory Server and handle database cleanup.
    - Modified tsconfig.json to include Jest types and include test files in the compilation.

---
+++ auth/tests/setup.ts
@@ -0,0 +1,26 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+// import app from "../src/app";
+
+let mongoServer: MongoMemoryServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db?.collections();
+
+  if (collections) {
+    for (const collection of collections) {
+      await collection.deleteMany({});
+    }
+  }
+});
+
+afterAll(async () => {
+  mongoServer.stop();
+  await mongoose.connection.close();
+});
