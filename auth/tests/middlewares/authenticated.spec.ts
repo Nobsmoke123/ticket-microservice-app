b7e7b3ea2532ba9a9c5b380c1bfed41157ca992c
Nobsmoke123 <donaldakobundu@gmail.com>
2025-05-08 03:09:53 +0100

    test(auth): enhance authenticated middleware tests for invalid and valid JWT scenarios

--- auth/tests/middlewares/authenticated.spec.ts
+++ auth/tests/middlewares/authenticated.spec.ts
@@ -2,6 +2,7 @@ import { authenticated } from "../../src/middlewares/authenticated";
 import { getMockReq, getMockRes } from "@jest-mock/express";
 // import { TokenUtils } from "../../src/utils";
 import { NextFunction } from "express";
+import { TokenUtils } from "../../src/utils";
 
 describe("Authenticated Middleware", () => {
   it("should throw Unauthorized error when the jwt is missing", () => {
@@ -17,5 +18,46 @@ describe("Authenticated Middleware", () => {
     expect(() => authenticated(mockedReq, mockedRes, next)).toThrow(
       "Authentication token is missing"
     );
+  });
+
+  it("should throw unauthorised error when the jwt is invalid", () => {
+    const mockedReq = getMockReq({
+      session: {
+        jwt: "invalid-token",
+      },
+    });
+
+    const { res: mockedRes } = getMockRes();
+
+    const next: NextFunction = jest.fn();
+
+    jest.spyOn(TokenUtils, "verifyJwtToken").mockImplementation(() => {
+      throw new Error("Invalid token");
+    });
+
+    expect(() => authenticated(mockedReq, mockedRes, next)).toThrow(
+      "Invalid token"
+    );
+  });
+
+  it("should call next when the jwt is valid", () => {
+    const mockedReq = getMockReq({
+      session: {
+        jwt: "valid-token",
+      },
+      user: "",
+    });
+
+    const { res: mockedRes } = getMockRes();
+
+    const next: NextFunction = jest.fn();
+
+    jest.spyOn(TokenUtils, "verifyJwtToken").mockReturnValue({
+      userId: "user-id",
+      email: "test-email",
+    });
+
+    authenticated(mockedReq, mockedRes, next);
+    expect(next).toHaveBeenCalled();
   });
 });
